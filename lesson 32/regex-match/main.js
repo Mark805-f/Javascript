0a54bd80b73df866d2b55844adaa59ef744ed29a
Mark805-f <fronbekteshi12@gmail.com>
2026-09-15 19:06:53 +0200

    add

---
+++ lesson 32/regex-match/main.js
@@ -0,0 +1,5 @@
+var text = "My school is the best school";
+var regex  = /school/g;
+
+var result = text.match(regex);
+document.getElementById('result5').innerHTML = result
---
+++ lesson 33/number-validation-age/main.js
@@ -0,0 +1,30 @@
+function validation() {
+    var name = document.getElementById("name").value;
+
+    var valid_name_regex = /^[A-Za-z]+$/;
+
+    var age = document.getElementById("age").value;
+    var valid_age_regex = /^[0-9]+$/;
+
+    if (!(name.match(valid_name_regex))) {
+        document.getElementById('name_error').style.visibility = "visible";
+        document.getElementById("name").style.borderColor = 'red';
+    } else {
+        document.getElementById('name_error').style.visibility = "hidden";
+        document.getElementById("name").style.borderColor = 'black';
+    }
+
+    if (!(age.match(valid_age_regex))) {
+        document.getElementById('age_error').style.visibility = "visible";
+        document.getElementById("age").style.borderColor = 'red';
+    } else {
+        document.getElementById('age_error').style.visibility = "hidden";
+        document.getElementById("age").style.borderColor = 'black';
+    }
+
+    if (!(name.match(valid_name_regex)) || !(age.match(valid_age_regex)) ) {
+        return false;
+    } else {
+        return true;
+    }
+}
